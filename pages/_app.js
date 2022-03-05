127c98dda4c1bbdb042a8ffbb1aafacb2a30f475
anohene1 <anoheneisaac@gmail.com>
2022-03-05 22:45:41 +0000

    complete header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Header from "../components/Header/Header";
+
+const Layout = ({ children }) => <div className="layout">{children}</div>;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Header />
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;

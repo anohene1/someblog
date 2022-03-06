85b77199512b3f756a3ad9383ba6b842286edad8
anohene1 <anoheneisaac@gmail.com>
2022-03-06 17:56:13 +0000

    complete footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import "../styles/globals.css";
 import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 const Layout = ({ children }) => <div className="layout">{children}</div>;
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Header />
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Header />
+        <Component {...pageProps} />
+      </Layout>
+      <Footer />
+    </>
   );
 }
 

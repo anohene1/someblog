eab2a2f359ff46bd8e2b8b166dd6e05e13432fdf
anohene1 <anoheneisaac@gmail.com>
2022-03-06 15:56:09 +0000

    complete hero

---
+++ components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./Hero.module.scss";
+
+function Hero() {
+  return (
+    <div className={styles.hero}>
+      <div>
+        <h1 className={styles.title}>Make better coffee ☕️</h1>
+        <h2>why learn how to blog?</h2>
+      </div>
+      <Image src="/../public/images/hero.png" width={536} height={383} />
+    </div>
+  );
+}
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header/Header";
 import Head from "next/head";
+import Hero from "../components/Hero/Hero";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Head>
         <title>SomeBlog</title>
       </Head>
+      <Hero />
     </div>
   );
 }
